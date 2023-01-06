f4f700d9c00c203cc5079dc0d1f988ac2c82f97f
her_username <sergey.orlov@materna.de>
2023-01-06 14:16:01 +0100

    added KIService

---
+++ MauMauSpiel/VirtuellerSpielerVerwaltung/src/main/java/de/htwberlin/kbe/gruppr7/MauMauSpiel/kiservice/export/KIService.java
@@ -0,0 +1,32 @@
+package de.htwberlin.kbe.gruppr7.MauMauSpiel.kiservice.export;
+
+
+import de.htwberlin.kbe.gruppe7.MauMauSpiel.kartenverwaltung.entity.Farbe;
+import de.htwberlin.kbe.gruppe7.spielerverwaltung.entity.Spieler;
+
+/**
+ *  Die Schnittstelle enthält die Methoden für die Verwaltung der virtuellen Spieler (KI-Spieler).
+ */
+public interface KIService {
+
+    /** Methode erzeugt einen randomen Namen für KI-Spieler.
+     * Ursprüngliche Code-Quelle der Methode: https://jvm-gaming.org/t/super-simple-name-generator/53855
+     * @return String der generierten Namen
+     */
+        public String kiNamenGenerieren();
+
+    /** Methode entscheidet zufällig für den KI-Spieler, welche Farbe er für sich wünscht.
+     * @return gewuenschte Farbe
+     */
+        public Farbe kiWuenschtFarbe();
+
+    /** Methode prüft, ob KI-Spieler "Mau" sagen muss und tut das auch richtig in allen Fällen.
+     * (d.h. in unserem Fall "vergisst" KI-Spieler nie "Mau" zu sagen)
+     * @param spieler Spieler, der geprüft werden soll
+     * @return boolescher Wert, der angibt, ob der KI-Spieler "Mau" gesagt hat
+     */
+        
+        
+        public boolean kiMauPruefung(Spieler spieler);
+    }
+
